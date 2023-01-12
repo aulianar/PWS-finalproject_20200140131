14b049661f786a60ae5b7dd7dd40a6c2f4232011
aulianar <arisky540@gmail.com>
2023-01-12 09:41:34 +0700

    membuat MyController

---
+++ src/main/java/ws/c/finalproject/finalproject/MyController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.c.finalproject.finalproject;
+
+/**
+ *
+ * @author USER DJOGJA
+ */
+public class MyController {
+    
+}
